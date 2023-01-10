0a318a993d699695cb223e8c0d81e3789256e23a
Ramakrishnan Lakshminarayan <122114368+laksram1204@users.noreply.github.com>
2023-01-09 19:32:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display table onthe page 
 streamlit.dataframe(fruits_to_show)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
